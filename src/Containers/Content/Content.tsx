b295d81b9ce49c9a2436b5290d30884183d3da5e
Ashymov Aziret <aziretashym@gmail.com>
2024-10-31 11:10:35 +0600

    Added loader.

--- src/Containers/Content/Content.tsx
+++ src/Containers/Content/Content.tsx
@@ -3,13 +3,18 @@ import { useParams } from 'react-router-dom';
 import axiosAPI from '../../AxiosAPI.ts';
 import { IPage } from '../../types';
 import Page from '../../Componets/Page/Page.tsx';
+import Loader from '../../Componets/Ui/Loader/Loader.tsx';
+
 
 const Content = () => {
   const params = useParams<{ pageName: string }>();
   const [page, setPage] = useState<IPage | null>(null);
+  const [loading, setLoading] = useState<boolean>(false)
+
 
   const fetchPageData = useCallback(async () => {
     try {
+      setLoading(true);
       const response = await axiosAPI<IPage>(`pages/${params.pageName}.json`);
       if (response.data) {
         setPage(response.data);
@@ -19,6 +24,7 @@ const Content = () => {
     } catch (e) {
       console.error(e);
     } finally {
+      setLoading(false);
     }
   }, [params.pageName]);
 
@@ -27,11 +33,18 @@ const Content = () => {
   }, [fetchPageData]);
 
 
-  return page ? (
-    <Page title={page.title} subtitle={page.subtitle} content={page.content} />
-  ) : (
-    <Page title="Page Not Found" subtitle="" content="" />
+  return (
+    <>
+      {loading ? <Loader /> : (
+        <>
+          {page ? <Page title={page.title} subtitle={page.subtitle} content={page.content} /> : (
+            <Page title="Page Not Found" subtitle="" content="" />
+          )}
+        </>
+      )}
+    </>
   );
+
 };
 
 export default Content;
--- src/Containers/EditPage/EditPage.tsx
+++ src/Containers/EditPage/EditPage.tsx
@@ -3,19 +3,24 @@ import { useNavigate } from 'react-router-dom';
 import axiosAPI from '../../AxiosAPI.ts';
 import PageForm from '../../Componets/PageForm/PageForm.tsx';
 import { IPage, IPageAPI } from '../../types';
+import Loader from '../../Componets/Ui/Loader/Loader.tsx';
 
 const EditPage: React.FC = () => {
   const [pages, setPages] = useState<IPageAPI | null>(null);
   const [selectedPage, setSelectedPage] = useState<string | null>(null);
   const [pageData, setPageData] = useState<IPage | null>(null);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false)
 
   const fetchPages = useCallback(async () => {
     try {
+      setLoading(true);
       const response = await axiosAPI.get<IPageAPI>('pages.json');
       setPages(response.data);
     } catch (e) {
       console.error(e);
+    } finally {
+      setLoading(false);
     }
   }, []);
 
@@ -38,25 +43,34 @@ const EditPage: React.FC = () => {
     e.preventDefault();
     if (selectedPage && pageData) {
       try {
+        setLoading(true);
         await axiosAPI.put(`pages/${selectedPage}.json`, pageData);
         navigate(`/pages/${selectedPage}`);
       } catch (e) {
         console.error(e);
+      } finally {
+        setLoading(false);
       }
     }
   };
 
   return (
-    <div className="container mt-5">
-      <h2 className="text-center mb-5">Edit page</h2>
-      <PageForm
-        selectedPage={selectedPage}
-        pageData={pageData}
-        onPageSelect={pageSelect}
-        onInputChange={inputChange}
-        onSave={save}
-      />
-    </div>
+    <>
+      {loading ? <Loader /> : (
+        <>
+          <div className="container mt-5">
+            <h2 className="text-center mb-5">Edit page</h2>
+            <PageForm
+              selectedPage={selectedPage}
+              pageData={pageData}
+              onPageSelect={pageSelect}
+              onInputChange={inputChange}
+              onSave={save}
+            />
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
